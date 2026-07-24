b9c670e6e379476194430baa1368075e36aa2f29
David Oweilayefa <kook28515@gmail.com>
2026-07-24 04:00:15 +0100

    Refactor response handling in signatures.js

--- api/signatures.js
+++ api/signatures.js
@@ -8,11 +8,10 @@ module.exports = async (req, res) => {
     try {
         if (fs.existsSync(filePath)) {
             const data = JSON.parse(fs.readFileSync(filePath, 'utf8'));
-            return res.status(200).json(data);
+            return res.json(data);
         }
-        return res.status(200).json([]);
-    } catch (error) {
-        console.error(error);
-        return res.status(200).json([]);
+        return res.json([]);
+    } catch (e) {
+        return res.json([]);
     }
 };
